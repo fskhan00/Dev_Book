9a4af4fbf2143bff9ec9faca1a95c36acf471ce7
fskhan00 <faisalshakoorkhan@gmail.com>
2020-06-05 04:40:33 +0100

    added user validation checks

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator/check");
 
 const router = express.Router();
 
-//@router   GET api/users
-//@desc     Test router
+//@router   POST api/users
+//@desc     Register User
 //@access   Public
 
-router.get("/", (req, res) => res.send("Users Route"));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more charecters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("Users Route");
+  }
+);
 
 module.exports = router;
